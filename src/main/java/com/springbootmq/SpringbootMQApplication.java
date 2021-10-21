3e20c48e2a386b11bf289c48f56e871d6aab55ec
reser001 <reserg666@gmail.com>
2021-10-21 20:37:12 +0800

    修改pom

--- src/main/java/com/springbootmq/SpringbootMQApplication.java
+++ src/main/java/com/springbootmq/SpringbootMQApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.springbootalldemo.dao")
     public class SpringbootMQApplication {
 
     public static void main(String[] args) {
